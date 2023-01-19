8dad8d180e141ec1d539b84d12b367096f49df50
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-01-19 01:50:18 +0100

    Update repl.ml

--- repl.ml
+++ repl.ml
@@ -1,5 +1,4 @@
-(* https://bernsteinbear.com/blog/lisp/00_fundamentals/ *)
-
+(* input stream *)
 type stream = { mutable line_num: int; mutable chr: char list; chan: in_channel };;
 
 (* match next char in input *)
@@ -20,7 +19,6 @@ let unread_char stm c = stm.chr <- c :: stm.chr;;
 (* trim whitespace *)
 let is_white c =
     c = ' ' || c = '\t' || c = '\n';;
-
 let rec eat_whitespace stm =
     (* skip whitespace chars *)
     let c = read_char stm in
@@ -34,10 +32,10 @@ let rec eat_whitespace stm =
 (* types *)
 type lobject =
     | Fixnum of int
+    | Boolean of bool
 
 (* interpreter *)
 exception SyntaxError of string;;
-
 let rec read_sexp stm =
     (* check if char is digit *)
     let is_digit c =
@@ -58,25 +56,43 @@ let rec read_sexp stm =
     eat_whitespace stm;
     (* read char into c *)
     let c = read_char stm in
-    (* number *)
-    if is_digit c
-    then read_fixnum (Char.escaped c)
+    (* number : note that ~ is negation *)
+    if (is_digit c) || (c = '~') then read_fixnum (Char.escaped (if c = '~' then '-' else c))
+    (* boolean *)
+    else if c = '#' then
+        match (read_char stm) with
+        | 't' -> Boolean(true)
+        | 'f' -> Boolean(false)
+        | x -> raise (SyntaxError ("Invalid boolean literal " ^ (Char.escaped x)))
     (* otherwise : error *)
     else raise (SyntaxError ("Unexpected char " ^ (Char.escaped c)));;
 
 (* repl *)
+let rec print_sexp e =
+    match e with
+    | Fixnum(v) -> print_int v
+    | Boolean(b) -> print_string (if b then "#t" else "#f")
 let rec repl stm =
     print_string "> ";
     (* flush stdout to avoid print after input *)
     flush stdout;
-    let Fixnum(v) = read_sexp stm in
+    (*let Fixnum(v) = read_sexp stm in*)
+    let sexp = read_sexp stm in
     (* print input *)
-    print_int v;
+    print_sexp sexp;
+    (*print_int v;*)
     (* print newline *)
     print_newline ();
     (* loop *)
     repl stm;;
 
+(* main *)
 let main =
     let stm = { chr=[]; line_num=1; chan=stdin } in
     repl stm;;
+
+
+
+
+
+
