ff22be77af885b1e6992fc49cb49fd8b85eb2c83
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-01-20 01:45:46 +0100

    Update repl.ml

--- repl.ml
+++ repl.ml
@@ -34,9 +34,12 @@ type lobject =
     | Fixnum of int
     | Boolean of bool
     | Symbol of string
+    | Nil
+    | Pair of lobject * lobject
 
-(* interpreter *)
+(* parser *)
 exception SyntaxError of string;;
+exception UnknownError;;
 (* alternative to Char.escaped *)
 let charToString c = String.make 1 c;;
 let rec read_sexp stm =
@@ -78,6 +81,17 @@ let rec read_sexp stm =
         if is_delimiter nc then let _ = unread_char stm nc in ""
         else charToString nc ^ read_symbol ()
     in
+    (* read list *)
+    let rec read_list stm =
+        eat_whitespace stm;
+        let c = read_char stm in
+        if c = ')' then Nil
+        else
+            let _ = unread_char stm c in
+            let car = read_sexp stm in
+            let cdr = read_list stm in
+            Pair(car, cdr)
+    in
     (* remove whitespace *)
     eat_whitespace stm;
     (* read char into c *)
@@ -92,15 +106,44 @@ let rec read_sexp stm =
         | 't' -> Boolean(true)
         | 'f' -> Boolean(false)
         | x -> raise (SyntaxError ("Invalid boolean literal " ^ (Char.escaped x)))
+    (* list *)
+    else if c = '(' then read_list stm
     (* otherwise : error *)
     else raise (SyntaxError ("Unexpected char " ^ (Char.escaped c)));;
 
 (* repl *)
 let rec print_sexp e =
+    (* match list *)
+    let rec is_list e =
+        match e with
+        | Nil -> true
+        | Pair(a, b) -> is_list b
+        | _ -> false
+    in
+    (* list or pair *)
+    let rec print_list l =
+        match l with
+        | Pair(a, Nil) -> print_sexp a;
+        | Pair(a, b) -> print_sexp a; print_string " "; print_list b;
+        | _ -> raise UnknownError;
+    in
+    (* pair *)
+    let print_pair p =
+        match p with
+        | Pair(a, b) -> print_sexp a; print_string " . "; print_sexp b
+        | _ -> raise UnknownError;
+    in
+    (* match types *)
     match e with
     | Fixnum(v) -> print_int v
     | Boolean(b) -> print_string (if b then "#t" else "#f")
     | Symbol(s) -> print_string s
+    | Nil -> print_string "nil"
+    | Pair(a, b) ->
+        print_string "(";
+        if is_list e then print_list e
+        else print_pair e;
+        print_string ")";;
 let rec repl stm =
     print_string "> ";
     (* flush stdout to avoid print after input *)
